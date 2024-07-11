d5834a7927daac704e0e0e78e7f6e2a1605dbd83
programmingElements <159023430+programmingElements@users.noreply.github.com>
2024-07-11 06:01:55 +0000

    edit, delete, add todos

--- todo-context/src/App.jsx
+++ todo-context/src/App.jsx
@@ -9,13 +9,19 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TODO":
           return [action.payload, ...state];
-
+    case "TOGGLE_TODO":
+          return state.map((todo) => todo.id === action.payload ? ({...todo, completed: !todo.completed}): todo);
+    case "EDIT_TODO":
+          return state.map((todo) => todo.id === action.payload.id ? ({...todo, title: action.payload.editedInput}) : todo);
+    case "DELETE_TODO":
+          return state.filter((todo) => todo.id !== action.payload);
     default :
           return state;
   }
 }
 
 function App() {
+
   const [todos, dispatch] = useReducer(reducer, initialState);
 
   const data = {
--- todo-context/src/components/Todo.jsx
+++ todo-context/src/components/Todo.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTodosContext } from "../utils/TodosContext";
 
 const Todo = ({todo}) => {
-    return <div>
-        <h3>{todo.title}</h3>
+    const {dispatch} = useTodosContext();
+    const [isEditable, setIsEditable] = useState(false);
+    const [editedInput, setEditedInput] = useState(todo.title);
+
+    const toggle_todo = (id) => {
+        dispatch({type: "TOGGLE_TODO", payload: id})
+    }
+
+    const edit_todo = (id) => {
+        if (!todo.completed) {
+            if (isEditable) {
+                dispatch({ type: "EDIT_TODO", payload: {id, editedInput }})
+                setIsEditable((prev) => !prev);
+            } else {
+                setIsEditable((prev) => !prev);
+            }
+        }
+    }
+
+    const delete_todo = (id) => {
+        dispatch({ type: "DELETE_TODO", payload: id });
+    }
+
+    return <div style={{display: "flex", gap: "8px", alignItems: "center", justifyContent: "center"}}>
+        <input type="checkbox" onChange={() => toggle_todo(todo.id)} />
+        <p>
+            {
+                isEditable ?  (<input type="text" value={editedInput} onChange={(e) => setEditedInput(e.target.value)} />) : (<h3 style={{textDecoration: `${ todo.completed ? 'line-through' : '' } `}}>{todo.title}</h3>)
+            }
+        </p>
+        <div style={{display: "flex", gap: "8px"}}>
+            <button onClick={() => edit_todo(todo.id)}>{ isEditable ? "SAVE" : "EDIT"}</button>
+            <button onClick={() => delete_todo(todo.id)}>DELETE</button>
+        </div>
     </div>
 }
 
